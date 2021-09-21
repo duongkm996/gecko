69be4d056f9cd04990d90f257b3eae8e32e81967
theduong <theduongkm96@gmail.com>
2021-09-21 18:06:28 +0700

    add logo

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useHistory } from 'react-router';
+import Logo from '../../images/haederlogo.svg';
 
 function MainHeader() {
     const history = useHistory();
@@ -17,7 +18,7 @@ function MainHeader() {
             <div className="main-header">
                 <nav className="navbar navbar-expand-lg">
                     <a className="navbar-brand" onClick={() => history.push('/')}>
-                        <img width="150" src="https://static.coingecko.com/s/coingecko-logo-d13d6bcceddbb003f146b33c2f7e8193d72b93bb343d38e392897c3df3e78bdd.png" alt="" />
+                        <img src={Logo} alt="Cryto Finance" />
                     </a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
