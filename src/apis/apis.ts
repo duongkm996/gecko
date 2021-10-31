38e4752c8597a31056c85db9dc9ee9ecea8b8414
theduong <theduongkm96@gmail.com>
2021-10-31 23:45:44 +0700

    update

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 
 export const getGlobal = async () => {
   try {
-    const result = await axios.get("http://207.148.127.143/api/coins/global", {
-      headers: {
-        Accept: "application/json",
-      },
-    });
+    const result = await axios.get(
+      "http://cryptofinance.life/api/coins/global",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
     const resultData = result.data;
     if (resultData.status === 200) {
       return resultData.result;
